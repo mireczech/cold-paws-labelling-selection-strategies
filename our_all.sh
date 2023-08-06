310c1fa28fcc6bbb3c520c80e5e7409058eb7ff0
Ubuntu <ubuntu@timelessgalilei-64268.denbi.local>
2023-08-06 13:40:58 +0000

    configs for different number of sampled data points

--- our_all.sh
+++ our_all.sh
@@ -3,21 +3,21 @@
 python main_setup.py
 
 # simclr
-python main_results.py --config config/our.yaml --dataset data_processed/matek_simclr_v1.pickle
-python main_results.py --config config/our.yaml --dataset data_processed/isic_simclr_v1.pickle
-python main_results.py --config config/our.yaml --dataset data_processed/retinopathy_simclr_v1.pickle
-python main_results.py --config config/our.yaml --dataset data_processed/jurkat_simclr_v1.pickle
-python main_results.py --config config/our.yaml --dataset data_processed/cifar10_simclr_v1.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/matek_simclr_v1.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/isic_simclr_v1.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/retinopathy_simclr_v1.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/jurkat_simclr_v1.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/cifar10_simclr_v1.pickle
 
 # swav
-python main_results.py --config config/our.yaml --dataset data_processed/matek_swav_v1.pickle
-python main_results.py --config config/our.yaml --dataset data_processed/isic_swav_v1.pickle
-python main_results.py --config config/our.yaml --dataset data_processed/retinopathy_swav_v1.pickle
-python main_results.py --config config/our.yaml --dataset data_processed/jurkat_swav_v1.pickle
-python main_results.py --config config/our.yaml --dataset data_processed/cifar10_swav_v1.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/matek_swav_v1.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/isic_swav_v1.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/retinopathy_swav_v1.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/jurkat_swav_v1.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/cifar10_swav_v1.pickle
 
 # dino
-python main_results.py --config config/our.yaml --dataset data_processed/matek_dino_v2.pickle
-python main_results.py --config config/our.yaml --dataset data_processed/isic_dino_v2.pickle
-python main_results.py --config config/our.yaml --dataset data_processed/jurkat_dino_v2.pickle
-python main_results.py --config config/our.yaml --dataset data_processed/cifar10_dino_v2.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/matek_dino_v2.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/isic_dino_v2.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/jurkat_dino_v2.pickle
+python main_results.py --config config/our_200.yaml --dataset data_processed/cifar10_dino_v2.pickle
